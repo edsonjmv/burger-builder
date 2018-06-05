d17a9fdc4c56870a3d097b8d92cd65606a3a7e5d
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-06-05 12:00:24 +0200

    Adding a dropdown component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -53,8 +53,8 @@ class ContactData extends Component {
         elementType: 'select',
         elementConfig: {
           options: [
-            {value: 'fastest', display: 'Fastest'},
-            {value: 'cheapest', display: 'Cheapest'}
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'}
           ]
         },
         value: ''
